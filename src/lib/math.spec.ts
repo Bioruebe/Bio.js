38bf91946856db210c081888b7db0a4a9bdee46c
Bioruebe <Bioruebe@users.noreply.github.com>
2023-12-23 18:51:12 +0100

    Added `clamp` function

--- src/lib/math.spec.ts
+++ src/lib/math.spec.ts
@@ -1,6 +1,64 @@
 import { describe, expect, test } from "vitest";
-import { roundFloat } from "./math";
+import { clamp, roundFloat } from "./math";
 
+
+describe("clamp", () => {
+	test("should return the number if between minimum and maximum", () => {
+		expect(clamp(1, 0, 2)).toBe(1);
+		expect(clamp(0, 0, 2)).toBe(0);
+		expect(clamp(2, 0, 2)).toBe(2);
+	});
+
+	test("should return the minimum if below the minimum", () => {
+		expect(clamp(0, 1, 3)).toBe(1);
+	});
+
+	test("should return the maximum if above the maximum", () => {
+		expect(clamp(3, 0, 2)).toBe(2);
+	});
+
+	test("should handle negative numbers", () => {
+		expect(clamp(-3, -2, 0)).toBe(-2);
+		expect(clamp(-2, -2, 0)).toBe(-2);
+		expect(clamp(-1, -2, 0)).toBe(-1);
+		expect(clamp(0, -2, 0)).toBe(0);
+		expect(clamp(1, -2, 0)).toBe(0);
+	});
+
+	test("should handle floating point numbers", () => {
+		expect(clamp(1.5, 0, 2)).toBe(1.5);
+		expect(clamp(1.5, 0, 1)).toBe(1);
+		expect(clamp(1.5, 0, 1.5)).toBe(1.5);
+		expect(clamp(1.5, 0, 1.4)).toBe(1.4);
+	});
+
+	test("should handle NaN", () => {
+		expect(clamp(NaN, 0, 2)).toBeNaN();
+	});
+
+	test("should handle Infinity", () => {
+		expect(clamp(Infinity, 0, 2)).toBe(2);
+		expect(clamp(-Infinity, 0, 2)).toBe(0);
+		expect(clamp(1, -Infinity, Infinity)).toBe(1);
+		expect(clamp(-Infinity, -Infinity, Infinity)).toBe(-Infinity);
+	});
+
+	test("should handle equal minimum and maximum values", () => {
+		expect(clamp(1, 1, 1)).toBe(1);
+	});
+
+	test("should throw an error if the minimum value is greater than the maximum value", () => {
+		expect(() => clamp(1, 2, 0)).toThrowError(RangeError);
+	});
+
+	test("should throw an error if the minimum value is not a number", () => {
+		expect(() => clamp(1, NaN, 0)).toThrowError(RangeError);
+	});
+
+	test("should throw an error if the maximum value is not a number", () => {
+		expect(() => clamp(1, 0, NaN)).toThrowError(RangeError);
+	});
+});
 
 describe("roundFloat", () => {
 	test("should round a positive number to the given amount of decimals", () => {
--- src/lib/math.ts
+++ src/lib/math.ts
@@ -1,4 +1,19 @@
 /**
+ * Clamp a number between a minimum and maximum value
+ * @param number The number to clamp
+ * @param min The minimum value
+ * @param max The maximum value
+ * @returns The clamped number
+ */
+export function clamp(number: number, min: number, max: number) {
+	if (min > max) throw new RangeError("Minimum value must be less than or equal to the maximum value");
+	if (isNaN(min)) throw new RangeError("Minimum value must be a number");
+	if (isNaN(max)) throw new RangeError("Maximum value must be a number");
+
+	return Math.max(min, Math.min(number, max));
+}
+
+/**
  * Round a float to the given amount of decimals
  * @param number The number to round
  * @param digits The amount of digits to round to
